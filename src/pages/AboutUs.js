bbf6b1069ee4874d1aa921ca2c4d9a4fb706305c
MichaelGetuWorku <michael.getu.hh3221@gmail.com>
2022-04-22 18:12:55 +0300

    Styled About Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServiceSection';
+import FaqSection from '../components/FaqSection';
+
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection /> 
+    </>
+  );
 };
 export default AboutUs;
